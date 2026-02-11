85950d55dc3f4ee77233545189d454312184959f
이유진 <125464850+janie71@users.noreply.github.com>
2026-02-11 17:40:52 +0900

    [Silver III] Title: 순열 사이클, Time: 236 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.py
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.py
@@ -1,23 +1,22 @@
 import sys
-from collections import deque
 T=int(input())
-
-def bfs(n):
-    q=deque()
-    q.append(n)
-    while q:
-        a=q.popleft()
-        visited[a] = 1
-        if visited[Seq[a]]==0:
-            q.append(Seq[a])
-
-for i in range(T):
-    count=0
+for _ in range(T):
     N=int(input())
-    Seq=[0]+list(map(int,sys.stdin.readline().split()))
-    visited = [0] * (N+1)
-    for j in range(1,N+1):
-        if visited[j]==0:
-            bfs(j)
-            count+=1
-    print(count)
+    List=[]
+    per=list(map(int,sys.stdin.readline().split()))
+    visited=[0]*N
+    res=0
+    for i in range(N):
+        #visited=0이면 새로 넣기
+        #넣고 per 값에 대해 루프
+        #per값이 visited=1일때까지
+        if visited[i]==0:
+            # List.append([i+1,per[i]])
+            res+=1
+            visited[i]=1
+            peri=per[i]
+            while visited[peri-1]==0:
+                #List[i+1].append([peri, per[peri]])
+                visited[peri-1]=1
+                peri=per[peri-1]
+    print(res)
