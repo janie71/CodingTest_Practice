de62723adfeeb1581a17b8ecadff7f65b7eed11c
이유진 <125464850+janie71@users.noreply.github.com>
2026-03-24 20:49:18 +0900

    [Gold V] Title: 넴모넴모 (Easy), Time: 3340 ms, Memory: 137556 KB -BaekjoonHub

--- 백준/Gold/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.py
+++ 백준/Gold/14712. 넴모넴모 （Easy）/넴모넴모 （Easy）.py
@@ -1,35 +1,30 @@
 import sys
-input = sys.stdin.readline
+#idx는 n*m을 1차원으로 바꾼 배열
+    #idx=지금 몇 번째 칸을 처리 중인지를 나타내는 1차원 인덱스
+    #idx = row * M + col
 
-N, M = map(int, input().split())
+def solve(idx):
+    global count
+    #끝까지 다 돌았을때
+    if idx == n * m:
+        count += 1
+        return
+    
+    row = idx // m
+    column = idx % m
 
-board = [[0] * M for _ in range(N)]
-res = 0
-
-def dfs(idx):
-    global res
-
-    # 모든 칸 다 본 경우
-    if idx == N * M:
-        res += 1
+    # 1. 넴모 안 놓는 경우
+    solve(idx + 1)
+    ## 놓으면 2×2 완성 → 금지
+    if row > 0 and column > 0 and graph[row-1][column] and graph[row][column-1] and graph[row-1][column-1]:
         return
 
-    r = idx // M
-    c = idx % M
+    graph[row][column] = 1 #넴모 놓기
+    solve(idx + 1)
+    graph[row][column] = 0 #넴모 복구
 
-    # 1. 현재 칸에 안 놓기
-    dfs(idx + 1)
-
-    # 2. 현재 칸에 놓기 (조건 체크)
-    # 2×2 검사
-    if r > 0 and c > 0:
-        if board[r-1][c] and board[r][c-1] and board[r-1][c-1]:
-            return  # 놓으면 2×2 완성 → 금지
-
-    # 놓기
-    board[r][c] = 1
-    dfs(idx + 1)
-    board[r][c] = 0  # 백트래킹
-
-dfs(0)
-print(res)
+n, m = map(int, sys.stdin.readline().split())
+graph = [[0] * m for _ in range(n)]
+count = 0
+solve(0)
+print(count)
